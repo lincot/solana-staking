dada71e24f966bdb754af6cb264959ccd88c0dbf
lincot <lincot@disroot.org>
2022-04-28 12:43:18 +0300

    error numeration

--- programs/staking-factory/src/error.rs
+++ programs/staking-factory/src/error.rs
@@ -2,15 +2,22 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum StakingError {
+    /// 6000 0x1770
+    #[msg("Overflow")]
     Overflow,
-    #[msg("Cannot change staking type, may only change conditions")]
-    CannotChangeStakingType,
-    #[msg("Stakes history must be provided")]
-    StakesHistory,
-    #[msg("There is nothing to claim")]
-    NothingToClaim,
-    #[msg("The unstake timelock has not yet expired")]
-    UnstakeTimelock,
+    /// 6001 0x1771
     #[msg("0 is not allowed as a value")]
     Zero,
+    /// 6002 0x1772
+    #[msg("Cannot change staking type, may only change conditions")]
+    CannotChangeStakingType,
+    /// 6003 0x1773
+    #[msg("Stakes history must be provided")]
+    StakesHistory,
+    /// 6004 0x1774
+    #[msg("There is nothing to claim")]
+    NothingToClaim,
+    /// 6005 0x1775
+    #[msg("The unstake timelock has not yet expired")]
+    UnstakeTimelock,
 }
